3510de9307fe40fef87e9cb0060d9382ffd5c2c7
pampi <pampi.com@gmail.com>
2016-06-11 23:15:42 +0200

    Naprawa karuzeli w panelu instruktora. @Iwomichu zrób rozwijaną wypłatę zamiast obenej w panelu instruktora.

--- templates/instruktorPanel.php
+++ templates/instruktorPanel.php
@@ -1,6 +1,6 @@
 <?php if (!empty($p)) { ?>
     <hr/>
-    <div class="row">
+    <div class="row" style="margin-bottom: 100px;">
         <div class="col-md-4 col-md-offset-4">
             <h3 class="jobs">Prowadzi:</h3>
             <div class="list-group jobs">
@@ -17,11 +17,12 @@
         </div>
     </div>
     <hr/>
+
     <div class="row">
         <div class="row" id="profile-month">
-            <div class="col-md-2" onclick="$('.carousel').carousel('prev'); $('.carousel').carousel('pause');"><h3><span
+            <div class="col-md-1" onclick="$('.carousel').carousel('prev'); $('.carousel').carousel('pause');"><h3><span
                         class="glyphicon glyphicon-chevron-left pull-left" aria-hidden="true"></span></h3></div>
-            <div class="col-md-8 profile-month-name">
+            <div class="col-md-10 profile-month-name">
                 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
                     <!-- Indicators -->
                     <ol class="carousel-indicators">
@@ -39,42 +40,10 @@
                     <div class="carousel-inner" role="listbox">
                         <?php $i = 0 ?>
                         <?php foreach ($p as $zajecie) { ?>
-                            <div class="item <?= ($i == 0) ? 'active' : '' ?>">
+                            <div class="item <?= ($i == 0) ? 'active' : '' ?>" style="font-size: large; min-height: 300px">
                                 <h3><?= $zajecie['nazwa_zajec'] ?></h3>
-                                <?php $razem += $zajecie['wynagrodzenieMiesieczne'] ?>
-                            </div>
-                            <?php $i++ ?>
-                        <?php } ?>
-                        <?php unset($i) ?>
-                    </div>
 
-                    <!-- Controls -->
-
-                </div>
-            </div>
-            <div class="col-md-2" onclick="$('.carousel').carousel('next'); $('.carousel').carousel('pause');"><h3><span
-                        class="glyphicon glyphicon-chevron-right pull-right" aria-hidden="true"></span></h3></div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-
-                        <?php $i = 0 ?>
-                        <?php foreach ($p as $zajecie) { ?>
-                            <li data-target="#carousel-example-generic" data-slide-to="<?= $i ?>"
-                                class="<?= ($i == 0) ? 'active' : '' ?>"></li>
-                        <?php } ?>
-                        <?php unset($i) ?>
-                    </ol>
-
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner" role="listbox">
-                        <?php $i = 0 ?>
-                        <?php foreach ($p as $zajecie) { ?>
-                            <div class="item <?= ($i == 0) ? 'active' : '' ?>">
-                                <table class="table text-center">
+                                <table class="table text-center"">
                                     <tr>
                                         <th class="text-center">Dzień tygodnia</th>
                                         <th class="text-center">Godzina</th>
@@ -93,6 +62,7 @@
                                     <?php } ?>
                                 </table>
                             </div>
+                            <?php $i++ ?>
                         <?php } ?>
                         <?php unset($i) ?>
                     </div>
@@ -101,6 +71,8 @@
 
                 </div>
             </div>
+            <div class="col-md-1" onclick="$('.carousel').carousel('next'); $('.carousel').carousel('pause');"><h3><span
+                        class="glyphicon glyphicon-chevron-right pull-right" aria-hidden="true"></span></h3></div>
         </div>
-    </div>
+
 <?php } ?>
