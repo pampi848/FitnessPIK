5fdbc34697c93a0b71a051df785acdf45f4d9c49
pampi <pampi.com@gmail.com>
2016-06-11 22:07:35 +0200

    Zmiany w 404, ponowne wysyłanie linku aktywacyjnego.

---
+++ Actions/SendAgainAC.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pampi
+ * Date: 11.06.16
+ * Time: 19:55
+ */
+
+namespace Actions;
+
+use Accounts\User;
+
+require_once "autoload.php";
+class SendAgainAC
+{
+    protected function doExecute()
+    {
+    
+    }
+}
---
+++ templates/error404.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pampi
+ * Date: 11.06.16
+ * Time: 20:17
+ */
