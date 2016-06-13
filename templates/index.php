c47ed87fc0574382fa0c897260be4eeae9d0b9d6
Iwomichu <caradornian.ajwo@gmail.com>
2016-06-14 00:48:16 +0200

    Gotowa strona główna

--- templates/index.php
+++ templates/index.php
@@ -1,2 +1,13 @@
-<?=isset($p) ? $p : '' ?>
-<?php include_once 'text.php' ?>
+<div class="row" style="margin-left: 0; margin-right: 0;">
+    <div class="col-md-12 main-site">
+        <div class="jumbotron title-main">
+            <h1>Zdobądź formę na lato</h1>
+            <h4>Zapisz się już dzisiaj na jeden z wielu rodzajów zajęć prowadzonych w Fortitudo Fitness Club</h4>
+            <div class="row">
+                <div class="col-md-12">
+                    <button type="button" class="pull-right btn btn-primary btn-lg">Zapisz się</button>
+                </div>
+            </div> 
+        </div>
+    </div>
+</div>
